c150921c02cb2db09297acfcb03a64132e2067c4
Alex Albu <alex.albu@equilobe.com>
2021-04-24 08:45:26 +0300

    step_3

--- exercises/jsl-wc/src/components/Achievement.ts
+++ exercises/jsl-wc/src/components/Achievement.ts
@@ -1,3 +1,23 @@
-import { LitElement } from 'lit-element';
+import { html, LitElement } from 'lit-element';
 
-export class Achievement extends LitElement {}
+import '../registry/badge.js';
+
+export class Achievement extends LitElement {
+  title: string = "Bronze User";
+
+  description: string = "Has posted more than 1 post on their profile";
+
+  progress: number = 100;
+
+  tooltip: string = "UNLOCKED!";
+
+  render() {
+    return html`
+      <jsl-badge></jsl-badge>
+      <div>${this.title}</div>
+      <div>${this.description}</div>
+      <div>${this.progress}</div>
+      <div>${this.tooltip}</div>
+    `;
+  }
+}
--- exercises/jsl-wc/src/components/Badge.ts
+++ exercises/jsl-wc/src/components/Badge.ts
@@ -1,3 +1,13 @@
-import { LitElement } from 'lit-element';
+import { html, LitElement } from 'lit-element';
 
-export class Badge extends LitElement {}
+export class Badge extends LitElement {
+  src: string = 'https://odindesignthemes.com/vikinger/img/badge/bronze-b.png';
+
+  alt: string = 'bronze smiley face';
+
+  render() {
+    return html`
+      <img src="${this.src}" alt="${this.alt}" />
+    `;
+  }
+}
